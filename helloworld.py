493ef849cfc3ba5a58391f268dcf35038ae9b22b
Teme Azanaw <tazanaw0@frostburg.edu>
2024-08-30 21:23:22 -0400

    First program | type, print functions and initializing a var w/ an int

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,4 @@
+#First python program comprised of a int variable, a print function w/ type function to get the var's data type and a print function outputting a string 'Hello World' 
 t = 9
 print(type(t))
 print('Hello, World')
