7108f254fcfa8c74f436a92b6fd343cc37307278
Teme <tazanaw0@frostburg.edu>
2024-09-29 17:49:07 -0400

    Checks user input for even or odd number | Input, if-else, print | Habit Formation

---
+++ September/092824.py
@@ -0,0 +1,14 @@
+#Exercise: Check for even or odd numbers
+
+#Asks the user to input a number between 1 and 100.
+##Checks if the number is even or odd.
+###Prints whether the number is even or odd
+
+#
+users_number = int(input('Please enter a number between 1 and 100: '))
+
+##
+if users_number % 2 == 0:
+    print(f'{users_number} is Even.')
+else:
+    print(f'{users_number} is Odd.')
