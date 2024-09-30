50bf44dadae18003a53a42af4c14233234be96bd
Teme <tazanaw0@frostburg.edu>
2024-09-30 13:48:45 -0400

    Quiz GUI w/ Tkinter | Create first window & configured title and dimensions

---
+++ quizApp/quiz_gui.py
@@ -0,0 +1,10 @@
+from tkinter import * ##Library used to develop GUI + interface to the Tk GUI toolkit
+
+#Tk(screenName=None, baseName=None, className='Tk', useTk=1)
+root = Tk()
+
+#Sets the title and dimensions of our first window
+root.title('2Min Quiz')
+root.geometry('350x200')
+
+root.mainloop()
