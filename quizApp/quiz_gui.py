330490a67935a1f088b27d83d4dec35f8b78a934
Teme <tazanaw0@frostburg.edu>
2024-10-02 12:54:08 -0400

    Fixed 'attribute label not found' issue w/ tkinter lib, added placeholder for current question & next question button | tk.Button and tk.Label

--- quizApp/quiz_gui.py
+++ quizApp/quiz_gui.py
@@ -8,18 +8,27 @@ q = Tk()
 q.title('2Min Quiz')
 q.geometry('350x200')
 
-#Create buttons
-start_button = Tk.Button(q, text='Start', width = 25, command = start_quiz)
-stop_button = Tk.Button(q, text='Stop', width = 25, command=q.destroy)
-#Infinite loop that runs until an enduser interacts w/ gui 
-q.mainloop()
+#Text presented to user when they open a window 
+start_label = Label(q, text='Welcome to 2Min Quiz App')
+start_label.pack() #Places label in center of gui window
+current_question = Label(q, text='Placeholder for current question')
+current_question.pack() # Places label in center of gui window 
 
-start_label = Tk.label(q, text='Welcome to 2Min Quiz App')
 #Changes label (welcome msg) in main window once a user clicks start button
 def start_quiz(): 
     start_label.config(text='Quiz starting...')
 
-
-start_label.pack()#Places our text in gui window
+#Create buttons
+start_button = Button(q, text='Start', width = 25, command = start_quiz)
 start_button.pack() #Places button in the center of our gui window 
+stop_button = Button(q, text='Stop', width = 25, command=q.destroy)
 stop_button.pack()#Places button in the center of our gui window 
+Next_question = Button(q, text='Next Question', width = 7) #Button user interacts w/ to access other questions
+Next_question.pack()
+
+#Infinite loop that runs until an enduser interacts w/ gui 
+q.mainloop()
+
+
+
+ 
