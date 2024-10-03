0128f4e5bc80d59eb69cfcb074af835a19d27bbc
Teme <tazanaw0@frostburg.edu>
2024-10-03 14:31:45 -0400

    Added questions, var to keep track, modified size of next/previous labels, & modified start_quiz function to display question | List, label attributes, functions,

--- quizApp/quiz_gui.py
+++ quizApp/quiz_gui.py
@@ -9,23 +9,33 @@ q.title('2Min Quiz')
 q.geometry('350x200')
 
 #Text presented to user when they open a window 
-start_label = Label(q, text='Welcome to 2Min Quiz App')
-start_label.pack() #Places label in center of gui window
-current_question = Label(q, text='Placeholder for current question')
-current_question.pack() # Places label in center of gui window 
+opening_label = Label(q, text='Welcome to 2Min Quiz App')
+opening_label.pack() #Places label in center of gui window
+question_label = Label(q, text='Placeholder for current question')
+question_label.pack() # Places label in center of gui window 
+
+#List of questions that we'll cycle through as the user interacts w/ gui. 
+Questions = ["Question 1: Which out of the following used to protect data in transit?",
+             "Question 2: What can organizations do to ensure compliance with local laws regarding data at rest?", 
+             "Question 3: What is used to protect sensitive information on receipts?"]
+#Establishes and associates a number to each question, allowing us to keep track of questions. 
+current_question = 0 
+
 
 #Changes label (welcome msg) in main window once a user clicks start button
 def start_quiz(): 
-    start_label.config(text='Quiz starting...')
+    opening_label.config(text='Quiz starting...')
+    question_label.config(text=Questions[current_question])
 
 #Create buttons
 start_button = Button(q, text='Start', width = 25, command = start_quiz)
 start_button.pack() #Places button in the center of our gui window 
 stop_button = Button(q, text='Stop', width = 25, command=q.destroy)
 stop_button.pack()#Places button in the center of our gui window 
-Next_question = Button(q, text='Next Question', width = 7) #Button user interacts w/ to access other questions
+Next_question = Button(q, text='Next Question', width = 10) #Button user interacts w/ to access other questions
 Next_question.pack()
-
+Previous_question = Button(q, text='Previous Question', width = 10) #Button user interacts w/ to access previous question
+Previous_question.pack()
 #Infinite loop that runs until an enduser interacts w/ gui 
 q.mainloop()
 
