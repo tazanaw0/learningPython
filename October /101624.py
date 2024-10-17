32d8d34ebd2638ae866a435c06707151f3319a0f
Teme <tazanaw0@frostburg.edu>
2024-10-17 18:51:41 -0400

    + toppings var to separate *args with comma + pass tuple values, + if else and nested for loop for delivery_info, output details in the key:value format used when calling the function (separates arguments onto their own line)

--- October /101624.py
+++ October /101624.py
@@ -2,7 +2,13 @@
 # Create a Python function to simulate placing an order for a pizza. 
 
 def order_pizza(pizza_type, *extratop, **delivery_info):
-    print(f"So, you've ordered {pizza_type}, with the following extra toppings {extratop}.")
-    print("Here are the details associated with your order", delivery_info)
+    toppings = ', '.join(extratop) if extratop else "No extra toppings." #Joins toppings into readable tuple(as opposed to outputting tuple syntax)
+    print(f"So, you've ordered {pizza_type}, with the following extra toppings: {toppings}.")
+    if delivery_info:
+        print("Here are the details associated with your order:")
+        for key, value in delivery_info.items(): #Outputs key:value pair on separate lines.
+            print(f"{key.capitalize()}: {value}")
+    else:
+        print("No delivery details supplied with order.")
 
 order_pizza("Cheese", "extra cheese", "green peppers", address = "123 Sally st.", phone_num = '123-123-12345')
