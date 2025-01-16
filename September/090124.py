693471549930353ee8a9779da4aac6abc53e72d4
Teme <tazanaw0@frostburg.edu>
2025-01-16 18:15:48 -0500

    Added round function + f strings

--- September/090124.py
+++ September/090124.py
@@ -2,7 +2,7 @@
 #Discount system based on customer's purchase amount for small e-comm platform
 
 #Takes user input and saves it as an int as we are expecting users to enter a number (amount spent)
-purchase_amount = int(input('Kindly type and enter in the amount you\'ve spent today: '))
+purchase_amount = float(input('Kindly type and enter in the amount you\'ve spent today: '))
 originalPrice = purchase_amount ##Unnecessary, since we are not changing the var locally w/ in the if-else blocks
 #Apply discount based on input 
 twenty_discount = '20%'
@@ -11,23 +11,21 @@ five_discount = '5%'
 #Checks user input w/ 4 conditions, outputs their original price, the discount applied, and the final price after a discount is applied. 
 if purchase_amount > 500:
     discount = purchase_amount * .20
-    finalPrice = purchase_amount - discount
-    print('Original purchase amount ->', originalPrice)
+    finalPrice = round(purchase_amount - discount, 2) 
+    print(f'Original purchase amount -> ${originalPrice}')
     print('Discount applied ->', twenty_discount)
     print(f'Price w/ discount -> ${finalPrice}')
 elif 200 <= purchase_amount <= 500:
     discount = purchase_amount * .10
-    finalPrice = purchase_amount - discount
-    print('Original purchase Amount ->', originalPrice)
+    finalPrice = round(purchase_amount - discount, 2)
+    print(f'Original purchase Amount -> ${originalPrice}')
     print('Discount applied ->', ten_discount)
     print(f'Price w/ discount -> ${finalPrice}')
 elif 100 <= purchase_amount <= 199:
     discount = purchase_amount *.05
-    finalPrice = purchase_amount - discount
-    print('Original purchase amount ->', originalPrice) ### check to see if this outputs og price
+    finalPrice = round(purchase_amount - discount, 2)
+    print(f'Original purchase amount -> ${originalPrice}') ### check to see if this outputs og price
     print('Discount applied ->', five_discount)
     print(f'Price w/ discount -> ${finalPrice}')
 else:
     print('Your purchase doesn\'t qualify for a discount. Must be at least $100 spent :(')
-
-
